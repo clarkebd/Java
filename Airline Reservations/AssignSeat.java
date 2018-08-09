cd71cf1e5f9722648a225c3ff771b3acfd72d30f
Brian Clarke <15670716+clarkebd@users.noreply.github.com>
2018-08-09 16:12:22 -0400

    Breaking out to methods()

--- Airline Reservations/AssignSeat.java
+++ Airline Reservations/AssignSeat.java
@@ -1,11 +1,14 @@
-package AirplaneReservations;
-
-import java.util.Scanner;
-
 /**
  * @author Brian
  */
-public class AssignSeat {
+
+package AirplaneReservations;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AssignSeat
+{
 
     public void displayWelcome()
     {
@@ -21,37 +24,55 @@ public class AssignSeat {
         System.out.print("Choice? ");
 
         Scanner input = new Scanner(System.in);
-
         int area = input.nextInt();
 
         return area;
     }
 
-    public void checkSpaceAvailable(int desired)
-    {
-        boolean[] seat;
-        seat = new boolean[10];
     
-        System.out.printf("So you made it here with %d", desired);
-        
-        if (desired==1)
-        {
+    public int bookFirst()
+    {
+            boolean[] first = new boolean[5];
+            
+            // testing purposes only
+            first[0]=true; // seat full
+            first[1]=true; // seat full
+            //first[2]=true; // seat full
+            //first[3]=true; // seat full
+            //first[4]=true; // seat full
+            
+            
             //check if space in first i.e. seats (0-4)
-            for (int loop=0;loop<10;loop++)
+            for (int loop=0;loop<5;loop++)
             {
-                if (seat[loop]==false)
+                if (first[loop]==false)
                 {
-                    System.out.print("Space is still available in First class.");
-                    int spaceAvailableInFirst=1;
+                    System.out.printf("Seat %d still available in First Class. Booking it for you!\n", loop);
+                    first[loop]=true;
+                    return loop;
                 } 
             }
- 
-        }
-        else
-        {
-            // check if space in couch i.e. seats 5-9
-            System.out.print("I'm down in the else section.");
-        }
-        
+        System.out.print(Arrays.toString(first));
+        return 0;
     }
+    
+
+    public int bookCoach()
+    {
+            boolean[] coach = new boolean[5];
+            
+                 //check if space in coach i.e. seats (5-9)
+                for (int loop=0;loop<5;loop++)
+                {
+                    if (coach[loop]==false)
+                    {
+                        System.out.printf("Seat %d still available in Coach Class.Booking it for you!\n", loop);
+                        coach[loop]=true;
+                        return loop;
+                    } 
+                } 
+          System.out.print(Arrays.toString(coach));            
+          return 0;                
+    }
+
 }
