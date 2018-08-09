db0290a91622b27c8971b5dfea9478b992f5b321
Brian Clarke <15670716+clarkebd@users.noreply.github.com>
2018-08-09 16:10:11 -0400

    Breaking out to methods()

---
+++ Airline Reservations/checkSpaceAvailable.java
@@ -0,0 +1,22 @@
+
+package AirplaneReservations;
+
+import java.util.Scanner;
+
+/**
+ * @author Brian
+ */
+public class checkSpaceAvailable
+{
+    public void checkSpaceAvailable(int desired)
+        {
+          // check if space in coach i.e. seats 5-9
+                System.out.print("Sorry, First class is full.");
+                System.out.print("Would you like to try a seat in Coach?");
+                System.out.print("1 - Yes\n");
+                System.out.print("2 - No\n");
+                System.out.print("Choice? ");
+
+                Scanner input = new Scanner(System.in);    
+    }         
+}
